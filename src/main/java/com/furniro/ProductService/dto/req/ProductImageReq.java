c7068e185125ed252104970182cfe636aeeaecb2
lamdoanh2468 <lamdoah2468@gmail.com>
2026-04-24 11:25:10 -0700

    fix database source url

--- src/main/java/com/furniro/ProductService/dto/req/ProductImageReq.java
+++ src/main/java/com/furniro/ProductService/dto/req/ProductImageReq.java
@@ -8,7 +8,7 @@ import lombok.AccessLevel;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ProductImageReq {
 
-    // can has not id (when create) and has id (when update)
+    // can have not id (when create) and have id (when update)
     private Integer id;
 
     private Integer productID;
